76926094473fc89b957c0346700898872721eba1
wangdunwei <wangdunwei@wxbtc.com>
2018-10-18 14:59:20 +0800

    1.更新design 2.更新gradle

---
+++ constraintlayoutdemo/src/main/java/com/ayvytr/constraintlayoutdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ayvytr.constraintlayoutdemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_cl3)
+    }
+}
